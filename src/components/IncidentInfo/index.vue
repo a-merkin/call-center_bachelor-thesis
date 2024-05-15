87668b1debb93b0a119326c4ed8a36c13f8d2318
Меркин Арсен <arsen.merkin@gmail.com>
2024-05-15 22:14:14 +0500

    reworked incident info panel

--- src/components/IncidentInfo/index.vue
+++ src/components/IncidentInfo/index.vue
@@ -6,24 +6,29 @@
         <el-date-picker v-model="incident.date" type="date" placeholder="Выберите дату" class="incident-info__input"></el-date-picker>
       </div>
       <div class="incident-info__item">
-        <label class="incident-info__label">ФИО заявителя:</label>
-        <el-input v-model="incident.applicantName" placeholder="Введите ФИО заявителя" class="incident-info__input"></el-input>
+        <label class="incident-info__label">ФИО:</label>
+        <el-input v-model="incident.fio" placeholder="Введите ФИО" class="incident-info__input"></el-input>
       </div>
       <div class="incident-info__item">
-        <label class="incident-info__label">Опасность для окружающих:</label>
-        <el-switch v-model="incident.dangerous" inactive-color="#13ce66" active-color="#ff4949" class="incident-info__switch"></el-switch>
+        <label class="incident-info__label">Телефон:</label>
+        <el-input v-model="incident.phone" placeholder="Введите номер телефона" class="incident-info__input"></el-input>
       </div>
       <div class="incident-info__item">
-        <label class="incident-info__label">Описание инцидента:</label>
-        <el-input v-model="incident.description" type="textarea" rows="3" placeholder="Введите описание инцидента" class="incident-info__input"></el-input>
+        <label class="incident-info__label">Группа/Школа:</label>
+        <el-input v-model="incident.groupSchool" placeholder="Введите группу или школу" class="incident-info__input"></el-input>
       </div>
       <div class="incident-info__item">
-        <label class="incident-info__label">Требуемые службы:</label>
-        <el-checkbox-group v-model="incident.requiredServices" class="incident-info__checkbox-group">
-          <el-checkbox label="Полиция"></el-checkbox>
-          <el-checkbox label="Скорая помощь"></el-checkbox>
-          <el-checkbox label="Пожарные"></el-checkbox>
-        </el-checkbox-group>
+        <label class="incident-info__label">Описание обращения:</label>
+        <el-input v-model="incident.reason" type="textarea" rows="3" placeholder="Введите описание обращения" class="incident-info__input"></el-input>
+      </div>
+      <div class="incident-info__item">
+        <label class="incident-info__label">Статус:</label>
+        <el-select v-model="incident.status" placeholder="Выберите статус" class="incident-info__input">
+          <el-option label="Новое" value="new"></el-option>
+          <el-option label="В работе" value="in-progress"></el-option>
+          <el-option label="Выполнено" value="completed"></el-option>
+          <el-option label="Отменено" value="cancelled"></el-option>
+        </el-select>
       </div>
     </el-card>
   </div>
@@ -34,10 +39,11 @@ import { defineComponent, ref } from 'vue';
 
 interface Incident {
   date: string;
-  applicantName: string;
-  dangerous: boolean;
-  description: string;
-  requiredServices: string[];
+  fio: string;
+  phone: string;
+  groupSchool: string;
+  reason: string;
+  status: 'new' | 'in-progress' | 'completed' | 'cancelled';
 }
 
 export default defineComponent({
@@ -45,10 +51,11 @@ export default defineComponent({
   setup() {
     const incident = ref<Incident>({
       date: '',
-      applicantName: '',
-      dangerous: false,
-      description: '',
-      requiredServices: []
+      fio: '',
+      phone: '',
+      groupSchool: '',
+      reason: '',
+      status: 'new'
     });
 
     return { incident };
@@ -60,7 +67,8 @@ export default defineComponent({
 .incident-info {
   &__card {
     margin-bottom: 20px;
-    height: 100%
+    width: 100%;
+    height: 100%;
   }
 
   &__item {
@@ -70,19 +78,11 @@ export default defineComponent({
   &__label {
     font-weight: bold;
     display: block;
+    margin-bottom: 5px;
   }
 
   &__input {
     width: 100%;
-  }
-
-  &__switch {
-    margin-top: 5px;
-  }
-
-  &__checkbox-group {
-    display: flex;
-    flex-direction: column;
   }
 }
 </style>
